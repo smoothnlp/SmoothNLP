8024961a3c0efcfb78fba898fcd47ace0e5b882e
Ruinan Zhang <victor@Ruinans-MacBook-Pro.local>
2019-01-27 00:11:27 +0800

    add requirements

--- setup.py
+++ setup.py
@@ -13,6 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/zhangruinan/SmoothNLP",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'pytreebank',
+        'stanfordcorenlp'
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
