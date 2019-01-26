ac071a503ddc47262aaee1c2364d96380cd45aa8
Ruinan Zhang <victor@Ruinans-MacBook-Pro.local>
2019-01-26 18:37:51 +0800

    fix encode issue

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    long_description = fh.read().encode('utf-8')
 
 setuptools.setup(
     name="SmoothNLP",
