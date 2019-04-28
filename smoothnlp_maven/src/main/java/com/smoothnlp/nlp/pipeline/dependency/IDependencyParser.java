86b6cd9709ce580884ca600c8115554d9b903bad
Ruinan Zhang <victor@Ruinans-MacBook-Pro.local>
2019-04-28 12:04:58 +0800

    update dp interface

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/pipeline/dependency/IDependencyParser.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/pipeline/dependency/IDependencyParser.java
@@ -13,8 +13,8 @@ public interface IDependencyParser {
      * @throws Error
      */
 
-    public List<DependencyRelationship> parse(String input) throws Error;
+    public List<DependencyRelationship> parse(String input) throws Exception;
 
-    public List<DependencyRelationship> parse(List<SToken> stokens) throws Error;
+    public List<DependencyRelationship> parse(List<SToken> stokens) throws Exception;
 
 }
