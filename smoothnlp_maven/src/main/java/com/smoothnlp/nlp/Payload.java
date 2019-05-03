86954ae39812b58bdba772f140003cffed1a7d95
Xuewen <lixuewen@creditx.com>
2019-05-03 22:49:35 +0800

    add spring boot support.

---
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/Payload.java
@@ -0,0 +1,15 @@
+package com.smoothnlp.nlp;
+
+public class Payload {
+
+    private final String content;
+
+    public Payload(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/SmoothNLP.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/SmoothNLP.java
@@ -12,7 +12,7 @@ import com.smoothnlp.nlp.pipeline.*;
 import com.smoothnlp.nlp.pipeline.dependency.DependencyRelationship;
 import com.smoothnlp.nlp.pipeline.dependency.IDependencyParser;
 import com.smoothnlp.nlp.pipeline.dependency.MaxEdgeScoreDependencyParser;
-import com.sun.org.apache.regexp.internal.RE;
+// import com.sun.org.apache.regexp.internal.RE;
 
 public class SmoothNLP{
 
@@ -25,7 +25,7 @@ public class SmoothNLP{
     public static IIOAdapter IOAdaptor = new ResourceIOAdapter();
 
     // static libraries
-    public static String[] libraries = new String[]{"financial_agencies.txt","financial_metrix.txt","financial_metrix_action.txt"};
+    public static String[] libraries = new String[]{"financial_agencies.txt","financial_metrix.txt","metrix_action.txt","organization_metrix.txt"};
 
     // static model files
     public static String CRF_SEGMENT_MODEL = "segment_crfpp.bin";
@@ -39,7 +39,8 @@ public class SmoothNLP{
     public static IEntityRecognizer NORMALIZED_NER = new NormalizedNER();
     public static IEntityRecognizer REGEX_NER = new RegexNER(new String[]{"financial_agencies","financial_agencies.txt",
                                                                             "financial_metrix","financial_metrix.txt",
-                                                                            "metrix_action","financial_metrix_action.txt"},true);
+                                                                            "organization_metrix","organization_metrix.txt",
+                                                                            "metrix_action","metrix_action.txt"},true);
 //    public static IEntityRecognizer STOKEN_NER = new RegexNER(new String[]{"STOPWORDS","stopwords.txt"},false);
 
 
@@ -63,11 +64,11 @@ public class SmoothNLP{
         return UtilFns.toJson(res);
     }
 
-    public static void main(String[] args) throws Exception{
-        System.out.println(process("纳斯达克100指数跌1%。纳指跌0.89%，标普500指数跌0.78%，道指跌约250点。"));
-        System.out.println(process("我买了十斤水果"));
-        System.out.println(process("国泰君安的估值去年上涨了百分之五十"));
+    // public static void main(String[] args) throws Exception{
+    //     System.out.println(process("纳斯达克100指数跌1%。纳指跌0.89%，标普500指数跌0.78%，道指跌约250点。"));
+    //     System.out.println(process("我买了十斤水果"));
+    //     System.out.println(process("国泰君安的估值去年上涨了百分之五十"));
 
-    }
+    // }
 
 }
