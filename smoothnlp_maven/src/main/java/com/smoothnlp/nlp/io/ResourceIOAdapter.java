f94b9087d12d82212084cebf019d5d6abbc80648
zhangruinan <ruinan.zhang@icloud.com>
2019-05-03 11:58:09 +0800

    update resource io adaptor, return file io is resources not found

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/io/ResourceIOAdapter.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/io/ResourceIOAdapter.java
@@ -1,5 +1,6 @@
 package com.smoothnlp.nlp.io;
 
+import com.smoothnlp.nlp.SmoothNLP;
 import com.typesafe.config.ConfigException;
 
 import java.io.*;
@@ -13,13 +14,12 @@ public class ResourceIOAdapter implements IIOAdapter
 
     public InputStream open(String path) throws IOException
     {
-//        InputStream is = this.getClass().getClassLoader().getResourceAsStream()
         InputStream is = ResourceIOAdapter.class.getClassLoader().getResourceAsStream(path);
         if (is == null)
         {
-            throw new FileNotFoundException("资源文件" + path + "不存在于jar中");
+            SmoothNLP.LOGGER.warning("资源文件" + path + "不存在于jar中");
+            return new FileIOAdapter().open(path);
         }
-
         return is;
     }
 
