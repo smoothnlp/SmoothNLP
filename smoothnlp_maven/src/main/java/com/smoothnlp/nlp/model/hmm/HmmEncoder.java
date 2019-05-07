3ac710304e02429e943435668ce9640ddc641b99
yjun1989 <yjun1989@gmail.com>
2019-05-07 18:16:04 +0800

    git add HmmEncoder.java

---
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/model/hmm/HmmEncoder.java
@@ -0,0 +1,9 @@
+package com.smoothnlp.nlp.model.hmm;
+
+public class HmmEncoder {
+    public enum Algorithm { FirstOrderHMM, SecondOrderHMM}
+
+    public boolean learn(String tempfile){
+        return false ;
+    }
+}
