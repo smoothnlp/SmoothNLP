e5e7ea2c5bd94a675422e60e3eda8d3222a173d9
yjun1989 <yjun1989@gmail.com>
2019-05-10 10:17:03 +0800

    change smoothnlp to server

--- smoothnlp/__init__.py
+++ smoothnlp/__init__.py
@@ -3,5 +3,6 @@ __name__ = "SmoothNLP"
 
 import smoothnlp.jvm as jvm
 from smoothnlp.jvm import initJVMConnection
-
-smoothnlp = jvm.LazyLoadingJClass("com.smoothnlp.nlp.SmoothNLP")
+from smoothnlp.server import smoothNlpRequest
+#smoothnlp = jvm.LazyLoadingJClass("com.smoothnlp.nlp.SmoothNLP")
+smoothnlp = smoothNlpRequest()
