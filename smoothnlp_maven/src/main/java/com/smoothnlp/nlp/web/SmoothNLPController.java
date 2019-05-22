482945ea1bfbaf9dc96ae69b26399c3c5e65eb39
zhangruinan <ruinan.zhang@icloud.com>
2019-05-22 11:55:04 +0800

    update corss origin access

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/web/SmoothNLPController.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/web/SmoothNLPController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class SmoothNLPController{
 
