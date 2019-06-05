df25def1047aea53dffd6b67a21e26439e9e5e53
zhangruinan <zhangr4@rose-hulman.edu>
2019-06-05 15:06:50 +0800

    remove redundant print statement

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/pipeline/dependency/CoNLLDependencyGraph.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/pipeline/dependency/CoNLLDependencyGraph.java
@@ -100,8 +100,8 @@ public class CoNLLDependencyGraph {
         ftrs.add((float) dependentIndex - targetIndex);  // 两者token之间的位置
 
         // 添加Embedding 特征
-        System.out.println(String.format("dependent token : %s",this.tokens[dependentIndex].getToken()));
-        System.out.println(String.format("target token : %s",this.tokens[targetIndex].getToken()));
+        //System.out.println(String.format("dependent token : %s",this.tokens[dependentIndex].getToken()));
+        //System.out.println(String.format("target token : %s",this.tokens[targetIndex].getToken()));
 
         float[] dependent_vec = SmoothNLP.WORDEMBEDDING_PIPELINE.process(this.tokens[dependentIndex].getToken());
         float[] target_vec = SmoothNLP.WORDEMBEDDING_PIPELINE.process(this.tokens[targetIndex].getToken());
