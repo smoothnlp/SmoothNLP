a7b69deef7c9bed9477169df53c9b807df097d96
zhangruinan <ruinan.zhang@icloud.com>
2019-09-04 17:26:39 +0800

    update for 0.2.16

---
+++ smoothnlp/server/__init__.py
@@ -0,0 +1,67 @@
+import requests
+from smoothnlp import HOST_URL
+
+class smoothNlpRequest(object):
+    def __init__(self):
+        self.url = HOST_URL
+
+    def set_url(self,url):
+        self.url = url
+
+    def __call__(self,text):
+        content = {"text":text}
+        r = requests.get(self.url, params=content)
+        self.result = r.json()['payload']['response']
+
+    def dependencyrelationships(self,text):
+        self.__call__(text)
+        return self.result['dependencyRelationships']
+
+    def ner(self,text):
+        self.__call__(text)
+        return self.result['entities']
+
+    def number_recognize(self,text):
+        entities = self.ner(text)
+        if entities is None :
+            return
+        numbers = []
+        for entity in entities:
+            if entity['nerTag'].lower() == "number":
+                numbers.append(entity)
+        return numbers
+
+    def company_recognize(self, text):
+        entities = self.ner(text)
+        if entities is None:
+            return
+        financial_agency = []
+        for entity in entities:
+            if entity['nerTag'].lower() == "company_name":
+                financial_agency.append(entity)
+        return financial_agency
+
+    def segment(self,text):
+        self.__call__(text)
+        return [v['token'] for v in self.result['tokens']]
+
+    def postag(self,text):
+        self.__call__(text)
+        return self.result['tokens']
+
+    def analyze(self, text):
+        self.__call__(text)
+        return self.result
+
+class smoothnlpDateRange(object):
+    def __init__(self):
+        self.url = HOST_URL + "/querydate"
+
+    def __call__(self, pubdate:str="", givendate:str=""):
+        content = {"givendate":givendate}
+        r = requests.get(self.url, params=content)
+        self.result = r.json()['payload']['response']
+
+    def getDateRange(self,pubdate,givendate):
+        self.__call__(pubdate,givendate)
+        return self.result
