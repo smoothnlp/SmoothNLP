c60ba7d5757e72ae710ac30d5cda0bbf5c1946be
zhangruinan <ruinan.zhang@icloud.com>
2019-11-04 16:18:39 +0800

    util中添加df写入db函数

--- smoothnlp/utils/__init__.py
+++ smoothnlp/utils/__init__.py
@@ -38,3 +38,38 @@ def convert(func):
         else:
             return json.loads(res)
     return toJson
+
+
+def to_sql_update(df, engine, schema, table, id_cols = None, chunksize=100):
+    """
+    将 pandas.DataFrame
+    :param df:
+    :param engine:
+    :param schema:
+    :param table:
+    :param id_cols:
+    :param chunksize:
+    :return:
+    """
+    if id_cols is None:
+        sql = ''' SELECT column_name from information_schema.columns
+                  WHERE table_schema = '{schema}' AND table_name = '{table}' AND
+                        COLUMN_KEY = 'PRI';
+              '''.format(schema=schema, table=table)
+        id_cols = [x[0] for x in engine.execute(sql).fetchall()]
+    else:
+        id_cols = id_cols
+    id_vals = [df[col_name].tolist() for col_name in id_cols]
+    counter=0
+    sql = ''' DELETE FROM {schema}.{table} WHERE 0 '''.format(schema=schema, table=table)
+    for row in zip(*id_vals):
+        sql_row = ' AND '.join([''' {}='{}' '''.format(n, v) for n, v in zip(id_cols, row)])
+        sql += ' OR ({}) '.format(sql_row)
+        counter+=1
+        if counter>=chunksize:
+            engine.execute(sql)
+            sql = ''' DELETE FROM {schema}.{table} WHERE 0 '''.format(schema=schema, table=table)
+            counter = 0
+    engine.execute(sql)
+
+    df.to_sql(table, engine, schema=schema, if_exists='append', index=False)
