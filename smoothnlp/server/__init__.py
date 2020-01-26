bd39134eed718bba842e18d0559c3e90171384e3
zhangruinan <ruinan.zhang@icloud.com>
2020-01-26 11:51:14 +0800

    fix segment meta handling bug

--- smoothnlp/server/__init__.py
+++ smoothnlp/server/__init__.py
@@ -102,7 +102,9 @@ class SmoothNLPRequest(object):
     def segment(self,text):
         result = _request(text)
         tokens = extract_meta(result, "tokens")
-        if isinstance(text,str) or isinstance(text,dict):
+        if tokens is None:
+            return []
+        elif isinstance(text,str) or isinstance(text,dict):
             tokens = [v['token']for v in tokens]
         else:
             tokens = [[v['token']for v in token_set] for token_set in tokens]
