ee05be48d34e81d0e0c61c81c3aee35dd212e76a
zhangruinan <ruinan.zhang@icloud.com>
2020-02-10 14:42:29 +0800

    清除不必要代码, 支持crfagu model

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/pipeline/PostagCRFPP.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/pipeline/PostagCRFPP.java
@@ -2,8 +2,13 @@ package com.smoothnlp.nlp.pipeline;
 
 import com.smoothnlp.nlp.SmoothNLP;
 import com.smoothnlp.nlp.basic.*;
-import com.smoothnlp.nlp.model.crfpp.ModelImpl;
-import com.smoothnlp.nlp.model.crfpp.Tagger;
+import com.smoothnlp.nlp.model.crfagu.ModelImpl;
+import com.smoothnlp.nlp.model.crfagu.Tagger;
+import com.smoothnlp.nlp.model.crfagu.TaggerImpl;
+
+//import com.smoothnlp.nlp.model.crfpp.ModelImpl;
+//import com.smoothnlp.nlp.model.crfpp.Tagger;
+//import com.smoothnlp.nlp.model.crfpp.TaggerImpl;
 
 import java.util.*;
 
@@ -11,19 +16,19 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import com.smoothnlp.nlp.basic.IDictionary.MatchResult;
 
+
+
 public class PostagCRFPP extends CRFModel{
     protected ModelImpl model;
-    protected ISequenceTagger segment_pipeline;
-    private List<String> libraryNames = null;
     private Pattern pupattern,numpattern;
-//    private Map<String,String> tokenLibrary;
     private IDictionary tokenLibrary,datetimeLibrary;
 
 
     public PostagCRFPP(){
         this.model = new ModelImpl();
-        this.model.open(SmoothNLP.CRF_POSTAG_MODEL,0,0,1.0);
-        this.pupattern  = Pattern.compile("[\\s]+|[+——！|，。/？、~@#￥%……&*（）()》《丨\\[\\]]+");
+        this.model.open(SmoothNLP.CRF_POSTAG_MODEL,0,1,1.0);
+
+        this.pupattern  = Pattern.compile("[\\s]+|[+——！“”|，。/？、~@#￥%……&*（）()》《丨\\[\\]]+");
         String numpattern = "[点两双一二三四五六七八九零十〇\\d.%十百千万亿]{2,8}";
         String old_numpattern = "[点两双一二三四五六七八九零十〇\\d|.|%|个|十|百|千|万|亿]+";
         this.numpattern = Pattern.compile(numpattern);
@@ -43,10 +48,6 @@ public class PostagCRFPP extends CRFModel{
         });
     }
 
-    public PostagCRFPP(ISequenceTagger segment_pipeline){
-        this.segment_pipeline = segment_pipeline;
-    }
-
     public String buildFtrs(String token, String[] ftrs){
 
         return token+"\t"+ join("\t",ftrs);
@@ -56,15 +57,8 @@ public class PostagCRFPP extends CRFModel{
         this.libraryNames = libraryNames;
     }
 
-//    public void setSegment_pipeline(ISequenceTagger segment_pipeline) {
-//        this.segment_pipeline = segment_pipeline;
-//    }
-
     public List<SToken> process(String input){
-        if (segment_pipeline==null){
-            segment_pipeline = new SegmentCRFPP();
-        }
-        List<SToken> stokens = segment_pipeline.process(input);
+        List<SToken> stokens = SmoothNLP.SEGMENT_PIPELINE.process(input);
         return process(stokens);
     };
 
@@ -77,34 +71,26 @@ public class PostagCRFPP extends CRFModel{
             return new ArrayList<SToken>();
         }else{
             for (SToken stoken: stokens){
-
-                String[] other_ftrs = new String[3];
-                other_ftrs[0] = Integer.toString(stoken.token.length());
-                other_ftrs[1] = Character.toString(stoken.token.charAt(0));
-                other_ftrs[2] = Character.toString(stoken.token.charAt(stoken.token.length()-1));
-
-                String ftr = super.buildFtrs(stoken.getToken(),other_ftrs);
+                // 额外特征
+//                String[] other_ftrs = new String[3];
+//                other_ftrs[0] = Integer.toString(stoken.token.length());
+//                String ftr = super.buildFtrs(stoken.getToken(),other_ftrs);
+                String ftr = super.buildFtrs(stoken.getToken());
                 tagger.add(ftr);
             }
             tagger.parse();
-            for (int i=0; i<stokens.size();i++){
+//            tagger.parseEmbedding();
+            for (int  i=0; i<stokens.size();i++){
 
                 String ytag = tagger.yname(tagger.y(i));  // predict的t
-
-                // 打印预测出的tag 和他们的概率, 用于debug
-//                System.out.print(stokens.get(i).token);
-//                System.out.print("\t");
-//                System.out.print(ytag);
-//                System.out.print("\t");
-//                System.out.println(tagger.prob(i));
+                double tagproba = tagger.prob(i);
 
                 // 对于字符串为"空格" 或者其他中文标点, 如"丨"的token, 强行改写postag = "PU"
                 Matcher pumatcher = this.pupattern.matcher(stokens.get(i).token);
                 while (pumatcher.find()){
                     if (pumatcher.end() - pumatcher.start() == stokens.get(i).token.length()) {
-//                        System.out.println("pu tag hit");
-//                        System.out.println(stokens.get(i).token);
                         ytag = "PU";
+                        tagproba = 1.0;
                     }
                 }
 
@@ -112,10 +98,12 @@ public class PostagCRFPP extends CRFModel{
                 while (numMatcher.find()){
                     if (numMatcher.end() - numMatcher.start() == stokens.get(i).token.length()){
                         ytag = "CD";
+                        tagproba = 1;
                     }
                 }
 
                 stokens.get(i).setPostag(ytag);
+                stokens.get(i).setTagproba(tagproba);
 
                 List<MatchResult> res = this.tokenLibrary.find(stokens.get(i).token);
                 for (MatchResult m: res){
@@ -138,17 +126,25 @@ public class PostagCRFPP extends CRFModel{
 
     public static void main(String[] args){
         ISequenceTagger s = new PostagCRFPP();
-//        System.out.println(s.process("五十块钱买了两个冰淇淋还是挺便宜的"));
-//        System.out.println(UtilFns.toJson(s.process("广汽集团上月利润达到5万,相比同期增长5%")));
-//        System.out.println(UtilFns.toJson(s.process("广汽集团上月利润达到5万 相比同期增长5%")));
-//        System.out.println(UtilFns.toJson(s.process("京东与格力开展战略合作丨家居要闻点评")));
-//        System.out.println(UtilFns.toJson(s.process("中国消费者协会")));
-//        System.out.println(UtilFns.toJson(s.process("上海市政府")));
-//        System.out.println(UtilFns.toJson(s.process("2015年windows10份额不到10%")));
-        System.out.println(UtilFns.toJson(s.process("阿里与腾讯达成合作协议")));
-        System.out.println(UtilFns.toJson(s.process("“试玩互动”获超过1千万元天使轮融资，游戏广告可“先试玩再下载”")));
-        System.out.println(UtilFns.toJson(s.process("这之后，百度还发布了好运中国年、AI办事处等十多个即将在2020年推出的超级IP，对用户的精细化运营正在为百度创造更大的商业空间。")));
-        System.out.println(UtilFns.toJson(s.process("首发|借助AI智能升级传统餐饮业 艾唯尔科技获东方富海数千万元Pre-A轮融资")));
+        System.out.println(s.process("五十块钱买了两个冰淇淋还是挺便宜的"));
+        System.out.println((s.process("广汽集团上月利润达到5万,相比同期增长5%")));
+        System.out.println((s.process("广汽集团上月利润达到5万 相比同期增长5%")));
+        System.out.println(s.process("一度被政策冷落的油电混合动力汽车，有可能被重新加注鼓励的法码。"));
+        System.out.println((s.process("“芬木科技”完成千万级人民币天使轮融资，投资方为思得投资、薛蛮子")));
+        System.out.println((s.process("据了解，三七互娱旗下首款云游戏已在开发当中，未来将登陆华为云游戏平台。")));
+        System.out.println((s.process("百度创新性地推出了基于搜索的营销推广服务")));
+        System.out.println((s.process("阿里与腾讯达成合作协议")));
+        System.out.println((s.process("预估值180亿 首钢股份获注迁安钢铁")));
+        System.out.println((s.process("“试玩互动”获超过1千万元天使轮融资，游戏广告可“先试玩再下载”")));
+        System.out.println((s.process("这之后，百度还发布了好运中国年、AI办事处等十多个即将在2020年推出的超级IP，对用户的精细化运营正在为百度创造更大的商业空间。")));
+        System.out.println((s.process("首发|借助AI智能升级传统餐饮业 艾唯尔科技获东方富海数千万元Pre-A轮融资")));
+        System.out.println((s.process("百度是全球最大的中文搜索引擎，2000年1月由李彦宏、徐勇两人创立于北京中关村，致力于向人们提供“简单，可依赖”的信息获取方式")));
+        System.out.println((s.process("据了解，三七互娱旗下首款云游戏已在开发当中，未来将登陆华为云游戏平台。")));
+        System.out.println((s.process("云计算将推动物联网产业发展")));
+        System.out.println((s.process("字节跳动与腾讯达成合作协议")));
+        System.out.println((s.process("嗅问将是上海文磨网络科技开发的一款信息整合产品")));
+        System.out.println((s.process("呼吸道飞沫传染是新型冠状病毒的主要传播途径")));
+        System.out.println((s.process("新款讴歌TL登上环保目录")));
     }
 
 }
