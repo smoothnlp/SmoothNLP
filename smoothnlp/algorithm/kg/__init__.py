c80b99d24a111afc9eb415df63ca6941f7c994ac
zhangruinan <ruinan.zhang@icloud.com>
2020-02-23 16:09:49 +0800

    支持pretty=False, 完善comment

--- smoothnlp/algorithm/kg/__init__.py
+++ smoothnlp/algorithm/kg/__init__.py
@@ -1,73 +1,47 @@
-from .phrase import adapt_struct,extract_noun_phrase,extract_verb_phrase,_get_rel_map
-from .event import extract_all_event,extract_action_event,extract_state_event
-from .entity import extract_subject,extract_object
-from .attr import extract_all_attr
 from ...nlp import nlp
-from ...configurations import config
-from functools import wraps
 import networkx as nx
+from ...server import _request
 
+def extract_all_kg(text,pretty:bool = True):
+    kg_result = _request(text,path="/kg/query",other_params={'pretty':pretty})
+    return kg_result
 
-@adapt_struct
-def extract_all_debug(struct:dict):
-    print(" ~~~~~ phrase ~~~~~~~")
-    print("noun(no describer): ", extract_noun_phrase(struct=struct, pretty=True, multi_token_only=False, with_describer=False))
-    print("noun(with describer): ",
-          extract_noun_phrase(struct=struct, pretty=True, multi_token_only=False, with_describer=True))
-    # print("describer: ", phrase.extract_mod_describer_phrase(struct = struct, pretty= True))
-    # print("vhybrid describer: ", phrase.extract_vhybrid_describer_phrase(struct=struct, pretty=True))
-    # print("loc describer: ", phrase.extract_loc_describer_phrase(struct=struct, pretty=True))
-    print("prep describer: ",phrase.extract_prep_describer_phrase(struct=struct, pretty=True))
-    # print("mod describer: ", phrase.extract_mod_describer_phrase(struct=struct, pretty=True))
+def extract(text,
+            pretty:bool = True):
+    """
+    对输入的 text 进行 知识图谱(N元组)抽取
+    :param text: 进行知识抽取的文本
+        支持格式-1: str, 超过一定长度的text会被自动切句
+        支持格式-2: [str], list of str
+    :param pretty: 是否对词组结果进行合并, 默认True
+        boolean: True/False
+    :return: 知识图谱(N-元组)
+        list
 
-    print("all describer: ", phrase.extract_all_describer_phrase(struct=struct, pretty=True))
+    Example:
+        extract("SmoothNLP在V0.3版本中正式推出知识抽取功能")
+        >> [{'_conf_score': 0.9187054, 'action': '正式推出', 'object': '知识抽取功能', 'subject': 'SmoothNLP', 'type': 'action'}]
 
-    print("verb: ", extract_verb_phrase(struct= struct, pretty=True))
-    print("num phrase: ",phrase.extract_num_phrase(struct = struct, pretty=True))
+    Todo: 支持nlp.analyze的raw结果作为Input
 
-    print(" ~~~~~ entity ~~~~~~~")
-    print("subject:", entity.extract_subject(struct=struct, pretty=True))
-    print("object: ",entity.extract_object(struct=struct,pretty=True))
-    print("tmod entity:", entity.extract_tmod_entity(struct, pretty=True))
-
-    print("~~~~~~~ attr ~~~~~~~~")
-    print("num attrs: ",attr.extract_attr_num(struct,pretty=True))
-
-    print("~~~~~~~~ event  ~~~~~~~~~")
-    print("subject&verb:", event.extract_subj_and_verb(struct=struct, pretty=True))
-    # print("prep event: ", event.extract_prep_event(struct=struct, pretty=True))
-    # print("tmod event: ", event.extract_tmod_event(struct=struct, pretty=True))
-    print("action event: ", event.extract_action_event(struct=struct, pretty=True))
-    print("state event: ", event.extract_state_event(struct=struct, pretty=True))
-    print("attr event: ", event.extract_attr_event(struct=struct, pretty=True))
-
-    print("~~~~~~~~ all  ~~~~~~~~~")
-    print("all: ", extract_all_kg(struct=struct, pretty=True))
-
-
-@adapt_struct
-def extract_all_kg(struct:dict, pretty:bool=True):
-    if (struct['dependencyRelationships'] is None):
-        config.logger.info(" sentence is too long for knowledge extraction ")
-        return []
-    events = extract_all_event(struct = struct, pretty = pretty)
-    return events
-
-def extract(text):
-    if isinstance(text,str) and len(text)>=50:
+    """
+    if isinstance(text,str):
         sents = nlp.split2sentences(text)
     elif isinstance(text,list):
         sents = text
     else:
-        sents = [text]
-    structs = [nlp.analyze(t) for t in sents if len(t)<180]
-    # structs = nlp.analyze(sents)
+        raise TypeError(" Unsupported type for text parameter: {}".format(text))
     all_kgs = []
-    for struct in structs:
-        all_kgs+=extract_all_kg(struct)
+    for sent in sents:
+        all_kgs+=extract_all_kg(text = sent, pretty = pretty)
     return all_kgs
 
 def rel2graph(rels:list):
+    """
+    依据多条知识图谱N元组构建Networkx类型的有向图
+    :param rels:
+    :return: nx.DiGraph
+    """
     g = nx.DiGraph()
     for rel in rels:
         g.add_node(rel['subject'])
@@ -84,6 +58,13 @@ def rel2graph(rels:list):
     return g
 
 def graph2fig(g,x:int=800,y:int=600):
+    """
+    用matplotlib对有向图进行可视化
+    :param g: nx.DiGraph
+    :param x: 像素
+    :param y: 像素
+    :return:
+    """
     if len(g)<=0: ## 处理空的Graph
         return
     import matplotlib.pyplot as plt
@@ -120,45 +101,3 @@ def graph2fig(g,x:int=800,y:int=600):
                                  font_size=16,
                                  font_family="SimHei")
     return fig
-
-
-struct2tokens = lambda struct: [t['token'] for t in struct['tokens']]
-
-@adapt_struct
-def extract_related(struct,ners= {"GS","COMPANY_REGISTR"},keywords=[],pretty=True):
-    all_kgs = extract_all_kg(struct = struct,pretty=False)
-    entities = struct['entities']
-    keywords = set(keywords)
-    tokens = struct['tokens']
-    keyword_index = set([t['index'] for t in tokens if t['token'] in keywords])
-    related_indexes = set([int(index) for ent in entities for index in ent['sTokenList'] if ent['nerTag'] in ners])
-    related_indexes=keyword_index.union(related_indexes)
-    related_kgs = []
-    for kg_piece in all_kgs:
-        val = kg_piece['subject']
-        # for val in kg_piece.values():
-        val_indexes = set([v['index'] for v in val])
-        if len(val_indexes.intersection(related_indexes))>0:
-            if pretty:
-                for k in kg_piece.keys():
-                    kg_piece[k] = phrase.prettify(kg_piece[k])
-            related_kgs.append(kg_piece)
-            break
-    return related_kgs
-
-
-@adapt_struct
-def get_paths(struct:dict=None):
-    rel_map = _get_rel_map(struct)
-    paths = []
-    def get_paths_helper(path,index=0):
-        if index not in rel_map:
-            paths.append(path)
-            return
-        rels = rel_map[index]
-        for rel in rels:
-            tem_path = path.copy()
-            tem_path.append(rel)
-            get_paths_helper(tem_path,rel['targetIndex'])
-    get_paths_helper([],0)
-    return paths
