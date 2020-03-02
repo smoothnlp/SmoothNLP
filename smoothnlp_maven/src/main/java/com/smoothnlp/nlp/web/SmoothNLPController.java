ba83d166277fccaae73b51a616166802e0432176
zhangruinan <ruinan.zhang@icloud.com>
2020-03-02 11:31:09 +0800

    update controller

--- smoothnlp_maven/src/main/java/com/smoothnlp/nlp/web/SmoothNLPController.java
+++ smoothnlp_maven/src/main/java/com/smoothnlp/nlp/web/SmoothNLPController.java
@@ -1,13 +1,17 @@
 package com.smoothnlp.nlp.web;
 
+import java.util.UUID;
 import com.smoothnlp.nlp.SmoothNLP;
 import com.smoothnlp.nlp.basic.SmoothNLPResult;
 
+import ml.dmlc.xgboost4j.java.XGBoostError;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.UUID;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
@@ -15,15 +19,41 @@ public class SmoothNLPController{
 
     @GetMapping("/")
     @ResponseBody
-    public SmoothNLPResult handle(@RequestParam(name="text", required=false, defaultValue="我买了十斤水果") String value){
+    public ResponseWrapper handle(@RequestParam(name="text", required=false, defaultValue="我买了十斤水果") String value) throws XGBoostError {
         SmoothNLPResult result;
-        try {
-            result = SmoothNLP.process(value);
-            return result;
-        } catch (Exception e) {
-            System.out.println(e);
-            return new SmoothNLPResult(e.toString());
+
+        result = SmoothNLP.process(value);
+        return new ResponseWrapper(result);
+
+    }
+
+    public class ResponseWrapper {
+
+        public Payload payload ;
+        public int status_code ;
+        public String msg;
+
+        public ResponseWrapper(Object resonse){
+            this.status_code = 0;
+            this.msg = "success";
+            this.payload = new Payload(resonse);
         }
+
+        public ResponseWrapper(Exception e){
+            this.status_code = 1;
+            this.msg = "error";
+            this.payload = new Payload(e.toString());
+        }
+
+        public class Payload{
+            public Object response;
+            public UUID request_id;
+            public Payload(Object response){
+                this.response = response;
+                this.request_id = UUID.randomUUID();
+            }
+        }
+
     }
 
 }
