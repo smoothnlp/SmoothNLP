ca6d8596e21d95c0c67f3e631aaba3bb3eab3c98
zhangruinan <ruinan.zhang@icloud.com>
2020-03-06 10:48:36 +0800

    add adjust for warning message in request server

--- smoothnlp/server/__init__.py
+++ smoothnlp/server/__init__.py
@@ -25,10 +25,10 @@ def _request_single(text, path, counter=0, max_size_limit=200, other_params:dict
         return _request_single(text, path=path, counter=counter, max_size_limit=max_size_limit)
     elif isinstance(result, dict) and "payload" in result:
         response =  result['payload']['response']
-        if ("tokens" not in response or response['tokens']==None) and ("subject" not in response):
-            counter+=1
-            config.logger.warn("{} failed to parse with tokens, reponse detail : {}".format(text,response))
-            return _request_single(text = text, path=path, counter=counter, max_size_limit=max_size_limit)
+        # if ("tokens" not in response or response['tokens']==None) and ("subject" not in response):
+        #     counter+=1
+        #     config.logger.warn("{} failed to parse with tokens, reponse detail : {}".format(text,response))
+        #     return _request_single(text = text, path=path, counter=counter, max_size_limit=max_size_limit)
         return response
     else:
         raise Exception(r.text)
